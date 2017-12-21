da913595434cf66adfc40ce572705bdd6724905a
Bruno Oliveira <bm.oliveira.dev@gmail.com>
2017-12-21 00:12:46 +0000

    Added configurations and fastlane scripts to deploy

---
+++ example-app/src/main/java/com/broliveira/example/extensions/ExampleActivity.kt
@@ -0,0 +1,12 @@
+package com.broliveira.example.extensions
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ExampleActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_example)
+  }
+}
